6571fb77f1480125d66a50c871ef2790db3b8aa8
Simon <ssmdmsn@hotmail.com>
2016-09-10 13:05:20 +0800

    index.php add header

---
+++ application/views/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+</head>
+<body>
+    <div id="header">
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+
+    </div>
+</body>
+</html>
