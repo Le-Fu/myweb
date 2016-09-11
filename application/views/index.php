12dfef7aa5259fc3fd631e03bd65a24babd26563
Simon <ssmdmsn@hotmail.com>
2016-09-11 23:07:57 +0800

    add some source files.

--- application/views/index.php
+++ application/views/index.php
@@ -3,11 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <title>首页</title>
+    <base href="<?php echo site_url();?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="My Skills Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        } </script>
 
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="js/jquery-1.11.1.min.js" ></script>
 </head>
 <body>
     <div id="header">
-
+        <div id="container">
+            <img src="img/p1.png" alt="">
+            <h1>hello..<span>!</span></h1>
+            <p>My name is Web & i do Creative Bots</p>
+            <a href="contact">
+                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+            </a>
+        </div>
     </div>
     <div id="content"></div>
     <div id="contact"></div>
