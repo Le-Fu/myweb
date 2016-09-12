cf905c129b75e13ef7f5a7b809fb11d9403ca1bf
Simon <ssmdmsn@hotmail.com>
2016-09-12 22:52:13 +0800

    I modified a some code.

--- application/views/index.php
+++ application/views/index.php
@@ -7,30 +7,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="My Skills Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
-    <script type="application/x-javascript"> addEventListener("load", function () {
+    <script type="application/x-javascript">
+        addEventListener("load", function () {
             setTimeout(hideURLbar, 0);
         }, false);
         function hideURLbar() {
             window.scrollTo(0, 1);
-        } </script>
-
+        }
+    </script>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/index.css">
     <script src="js/jquery-1.11.1.min.js" ></script>
 </head>
 <body>
     <div id="header">
-        <div id="container">
-            <img src="img/p1.png" alt="">
-            <h1>hello..<span>!</span></h1>
-            <p>My name is Web & i do Creative Bots</p>
-            <a href="contact">
-                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
-            </a>
+        <div class="wrapper">
+            <div class="logo">
+                <h1><a href=""><img src="img/logo.png" alt=""></a></h1>
+            </div>
+
+            <div id="nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Service</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <div id="search">
+                    <form action="">
+                        <input type="search" placeholder="Enter your search term...">
+                        <input type="submit" class="search-submit">
+                        <span></span>
+                        <!--                        <button type="submit"><img src="img/search.png" alt=""></button>-->
+                    </form>
+
+                </div>
+            </div>
+            <div class="clearfix"> </div>
         </div>
     </div>
-    <div id="content"></div>
-    <div id="contact"></div>
+    <div id="banner"></div>
+    <div id="service"></div>
+    <div id="meet"></div>
     <div id="footer"></div>
 </body>
 </html>
